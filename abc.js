48f49aeaa0e97f873b0a34cb7bc50eecb32b5c6c
Zixuan011125 <121881468+Zixuan011125@users.noreply.github.com>
2024-03-26 21:29:38 +0800

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,85 @@
+<script>
+var aAudio = new Audio('a.mp3');
+var bAudio = new Audio('b.mp3');
+var cAudio = new Audio('c.mp3');
+var dAudio = new Audio('d.mp3');
+var eAudio = new Audio('e.mp3');
+var fAudio = new Audio('f.mp3');
+var gAudio = new Audio('g.mp3');
+var hAudio = new Audio('h.mp3');
+var iAudio = new Audio('i.mp3');
+var jAudio = new Audio('j.mp3');
+var kAudio = new Audio('k.mp3');
+var lAudio = new Audio('l.mp3');
+var mAudio = new Audio('m.mp3');
+var nAudio = new Audio('n.mp3');
+var oAudio = new Audio('o.mp3');
+var pAudio = new Audio('p.mp3');
+var qAudio = new Audio('q.mp3');
+var rAudio = new Audio('r.mp3');
+var sAudio = new Audio('s.mp3');
+var tAudio = new Audio('t.mp3');
+var uAudio = new Audio('u.mp3');
+var vAudio = new Audio('v.mp3');
+var wAudio = new Audio('w.mp3');
+var xAudio = new Audio('x.mp3');
+var yAudio = new Audio('y.mp3');
+var zAudio = new Audio('z.mp3');
+
+
+function myAudioFunction(number){
+        if(number == '1'){
+            aAudio.play();
+        }else if(number == '2'){
+            bAudio.play();
+        }else if(number == '3'){
+            cAudio.play();
+        }else if(number == '4'){
+            dAudio.play();
+        }else if(number == '5'){
+            eAudio.play();
+        }else if(number == '6'){
+            fAudio.play();
+        }else if(number == '7'){
+            gAudio.play();
+        }else if(number == '8'){
+            hAudio.play();
+        }else if(number == '9'){
+            iAudio.play();
+        }else if(number == '10'){
+            jAudio.play();
+        }else if(number == '11'){
+            kAudio.play();
+        }else if(number == '12'){
+            lAudio.play();
+        }else if(number == '13'){
+            mnAudio.play();
+        }else if(number == '14'){
+            nAudio.play();
+        }else if(number == '15'){
+            oAudio.play();
+        }else if(number == '16'){
+            pAudio.play();
+        }else if(number == '17'){
+            qAudio.play();
+        }else if(number == '18'){
+            rAudio.play();
+        }else if(number == '19'){
+            sAudio.play();
+        }else if(number == '20'){
+            tAudio.play(); 
+        }else if(number == '21'){
+            uAudio.play();
+        }else if(number == '22'){
+            vAudio.play();
+        }else if(number == '23'){
+            wAudio.play();
+        }else if(number == '24'){
+            xAudio.play();
+        }else if(number == '25'){
+            yAudio.play();
+        }else if(number == '26'){
+            zAudio.play();
+        }   
+    }
+</script>
